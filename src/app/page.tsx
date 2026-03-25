abcc6df1f03184b9f890139417fea23062f6feb5
alex-correa-dev <alx.rcorrea@gmail.com>
2026-03-25 07:54:45 -0300

    fix layout issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-gray-100 font-sans dark:bg-black px-4 lg:px-16">
       <Header />
-      <main className="flex flex-1 w-full flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start mt-16">
+      <main className="flex flex-1 w-full flex-col items-center justify-between py-32 px-4 lg:px-16 bg-white dark:bg-black sm:items-start mt-16">
         <h1 className="text-3xl font-bold mb-8">Nossos Produtos</h1>
         <ProductsContainer />
       </main>
--- src/components/products/ProductListSkeleton.tsx
+++ src/components/products/ProductListSkeleton.tsx
@@ -6,7 +6,7 @@ import Skeleton from '@mui/material/Skeleton';
 
 export function ProductListSkeleton() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full">
       {[...Array(8)].map((_, index) => (
         <Card key={index} className="h-full">
           <Skeleton variant="rectangular" height={200} />
